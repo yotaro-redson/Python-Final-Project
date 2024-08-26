edaca6a531cf8bd42efb190fb175b3c8ea5f567e
yotaro-redson <yotaropn@gmail.com>
2024-08-26 21:05:45 +0700

    Commit

--- app.py
+++ app.py
@@ -1,47 +1,45 @@
 import streamlit as st
 import pickle
 import requests 
-movies=pickle.load(open("movies_list.pkl",'rb'))
-similarity =pickle.load(open("similarity.pkl",'rb'))
-movies_list=movies['title'].values
 
-st.header("Movie Reccomendation System")
-selectvalue=st.selectbox("Select movie from dropdown", movies_list)
+# Load data
+movies = pickle.load(open("movies_list.pkl", 'rb'))
+similarity = pickle.load(open("similarity.pkl", 'rb'))
+movies_list = movies['title'].values
+
+st.header("Movie Recommendation System")
+selectvalue = st.selectbox("Select movie from dropdown", movies_list)
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
-    data=requests.get(url)
-    data=data.json()
-    poster_path=data['poster_path']
-    full_path= "https://image.tmdb.org/t/p/w500/"+poster_path
-    return full_path
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check for HTTP errors
+        data = response.json()
+        poster_path = data.get('poster_path')
+        if poster_path:
+            return f"https://image.tmdb.org/t/p/w500/{poster_path}"
+        else:
+            return "https://via.placeholder.com/500x750?text=No+Poster+Available"
+    except (requests.RequestException, ValueError) as e:
+        # Handle any errors that occur during the API call or JSON parsing
+        print(f"Error fetching poster: {e}")
+        return "https://via.placeholder.com/500x750?text=Error+Loading+Poster"
 
 def recommand(movie):
-    index=movies[movies['title']==movie].index[0]
-    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
-    recommend_movie=[]
-    recommend_poster=[]
+    index = movies[movies['title'] == movie].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector: vector[1])
+    recommend_movie = []
+    recommend_poster = []
     for i in distance[1:6]:
-        movies_id=movies.iloc[i[0]].id
+        movie_id = movies.iloc[i[0]].id
         recommend_movie.append(movies.iloc[i[0]].title)
-        recommend_poster.append(fetch_poster(movies_id))
+        recommend_poster.append(fetch_poster(movie_id))
     return recommend_movie, recommend_poster
 
-if st.button("Show Reccomend"):
-    movie_name, movie_poster= recommand(selectvalue)
-    col1,col2,col3,col4,col5=st.columns(5)
-    with col1:
-        st.text(movie_name[0])
-        st.image(movie_poster[0])
-    with col2:
-        st.text(movie_name[1])
-        st.image(movie_poster[1])
-    with col3:
-        st.text(movie_name[2])
-        st.image(movie_poster[2])
-    with col4:
-        st.text(movie_name[3])
-        st.image(movie_poster[3])
-    with col5:
-        st.text(movie_name[4])
-        st.image(movie_poster[4])
+if st.button("Show Recommend"):
+    movie_name, _ = recommand(selectvalue)
+    
+    # Display each title in a separate row
+    for name in movie_name:
+        st.text(name)
